33e55c285e6b55a34e3a578d5df9e75c28b473d2
Wolf Wittmund <>
2019-02-10 11:29:07 +0100

    First Commit as test

---
+++ FavouriteBooks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FavouriteBooks
+//
+//  Created by Wolf Wittmund on 2019-02-10.
+//  Copyright © 2019 Wolf Wittmund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
